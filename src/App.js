ba327a5c98959674fabc6bad36f85f894be29afc
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-28 20:37:38 -0400

    had to change repos, big headache...ZZZZZ

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Search from "./components/Search";
+import Nav from "./components/Nav";
+import Layout from "./components/Layout";
+import EmployeeTable from "./components/EmployeeTable";
+
+function App() {
+  return (
+    <>
+
+      <Nav />
+      <Layout>
+      {/* two rows with one col, in the first col have a search, in the second col have an employee table */}
+          <Row >
+            <Col>
+              <Search></Search>
+            </Col>
+          </Row>
+          <Row >
+            <Col>
+              <EmployeeTable></EmployeeTable>
+            </Col>
+          </Row>
+      </Layout>
+
+
+    </>
+  );
+}
+
+export default App;
